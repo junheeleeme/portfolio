02dd0054996d43af03d69b68661319c4e390dfdc
junheelee <jhlee@infracube.co.kr>
2022-10-25 18:01:58 +0900

    chakra ui 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,32 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import Header from "../src/layout/header";
-import Main from "../src/layout/main";
-import Footer from "../src/layout/footer";
+import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+import type { AppProps } from 'next/app'
+import Header from '../src/layout/header'
+import Main from '../src/layout/main'
+import Footer from '../src/layout/footer'
 
 interface CustomAppProps extends AppProps {}
 
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+const theme = extendTheme({ colors })
+
 function MyApp({ Component, pageProps }: CustomAppProps) {
   return (
-    <>
+    <ChakraProvider theme={theme}>
       <Header />
       <Main>
         <Component {...pageProps} />
       </Main>
       <Footer />
-    </>
-  );
+    </ChakraProvider>
+  )
 }
 
-export default MyApp;
+export default MyApp
