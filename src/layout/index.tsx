47db721146e22a0c1f5afbc468b93385bebfa6ae
junheelee <jhlee@infracube.co.kr>
2022-12-13 17:23:15 +0900

    fixed: main 컴포넌트 페이지 이동효과 수정 및 리팩토링

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -11,6 +11,7 @@ import Footer from './footer'
 
 const Layout = ({ children }: React.PropsWithChildren) => {
   const router = useRouter()
+  // 상단 스크롤 훅
   const { scrollYProgress } = useScroll()
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 150,
@@ -23,21 +24,16 @@ const Layout = ({ children }: React.PropsWithChildren) => {
     <Box bg={bg} pos="relative" minW="100vw" minH="100vh" p="0 0 32px">
       {/* 스크롤 프로그레스바 */}
       <motion.div className="progress-bar" style={{ scaleX }} />
+      {/* ---------------- 헤더 ---------------- */}
       <Header />
+      {/* ------ 메인 섹션 페이지 이동 효과 ------ */}
       <AnimatePresence exitBeforeEnter>
-        <motion.main
-          key={router.route}
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 0.5 }}
-          transition={{
-            duration: 0.5,
-            ease: [0, 0.71, 0.5, 1],
-          }}
-        >
+        <motion.main {...motionProp} key={router.route}>
+          {/* ---------------- 메인 ---------------- */}
           <Main maxW="1280">{children}</Main>
         </motion.main>
       </AnimatePresence>
+      {/* ---------------- 푸터 ---------------- */}
       <Footer />
     </Box>
   )
@@ -58,5 +54,24 @@ const Layout = ({ children }: React.PropsWithChildren) => {
 //     opacity: 0,
 //   },
 // }
+
+const motionProp = {
+  initial: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+  },
+  exit: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  transition: {
+    duration: 0.3,
+    ease: [0, 0.71, 0.5, 1],
+  },
+}
 
 export default Layout
